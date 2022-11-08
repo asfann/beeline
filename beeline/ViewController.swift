2deef21509b2cceb7cc80006c36c7f16339324e1
asfann <64355174+asfann@users.noreply.github.com>
2022-11-08 16:44:09 +0600

    Initial Commit

---
+++ beeline/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  beeline
+//
+//  Created by Asf on 08.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
